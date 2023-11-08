47b7c36697f584f6e308ea9add4aaa9047e0a2e5
ViniLemess <viniciuslemescaetano@gmail.com>
2023-11-07 23:33:23 -0300

    Rename class to correct misspelling

--- src/main/java/com/vinilemess/dataanalyzer/exception/UnableToWrightFileException.java
+++
@@ -1,7 +0,0 @@
-package com.vinilemess.dataanalyzer.exception;
-
-public class UnableToWrightFileException extends RuntimeException{
-    public UnableToWrightFileException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/vinilemess/dataanalyzer/exception/UnableToWriteFileException.java
@@ -0,0 +1,7 @@
+package com.vinilemess.dataanalyzer.exception;
+
+public class UnableToWriteFileException extends RuntimeException{
+    public UnableToWriteFileException(String message) {
+        super(message);
+    }
+}
